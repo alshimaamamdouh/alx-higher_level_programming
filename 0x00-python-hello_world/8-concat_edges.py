976500ea3a9b35a14443b27b2db735cabbd3e7e8
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-10-30 08:34:11 -0700

    task

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+s = str[39:67]
+s1 = str[107:112]
+s2 = str[:6]
+str = s + s1 + s2
+print(str)
