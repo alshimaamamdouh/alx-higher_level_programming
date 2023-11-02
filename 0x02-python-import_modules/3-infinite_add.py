efeb75800ab2bd487b3660e5d3f118aeb9f52d71
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-02 15:05:54 -0700

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    j = 1
+    net = 0
+    for i in sys.argv:
+        if j <= len(sys.argv) - 1:
+            net = net + int( sys.argv[j])
+            j += 1
+    print(net)
