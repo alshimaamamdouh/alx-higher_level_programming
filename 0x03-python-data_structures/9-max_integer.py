a23c52abef5f18a4cca8a4a64ef32cca530756a0
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-06 15:17:51 -0800

    task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        tem = my_list[0]
+        for num in my_list:
+            if num > temp:
+                temp = num
+
+        return (temp)
+    else:
+        return (None)
