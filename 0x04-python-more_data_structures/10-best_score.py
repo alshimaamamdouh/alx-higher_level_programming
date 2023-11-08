60539f4a561e4edf7255878c288802dce630213a
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-08 15:10:17 -0800

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        temp = ""
+        temp_k = ""
+        temp_v = 0
         for k, v in a_dictionary.items():
-            if k > temp:
-                temp = k
-        return temp
+            if v > temp_v:
+                temp_k = k
+                temp_v = v
+        return temp_k
     else:
 
         return None
