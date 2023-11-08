50a8004efc5c82a7692478d5bb6bab6ebadcf456
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-08 14:45:28 -0800

    task9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    a_dictionary.update((k, v * 2) for k, v in a_dictionary.items())
-    return a_dictionary
+    new_dic = a_dictionary.copy()
+    new_dic.update((k, v * 2) for k, v in new_dic.items())
+    return new_dic
