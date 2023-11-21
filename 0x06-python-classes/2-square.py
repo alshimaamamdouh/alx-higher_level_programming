106c30e7bc4587ed7ccbc5720c9aac2749441be5
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-21 09:55:25 -0800

    task3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''square module'''
+
+
+class Square:
+    '''A class that represents a square'''
+    def __init__(self, size):
+        """Initialises the data"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def calculate_area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+        float: The area of the square.
+        """
+        return self.side_length ** 2
