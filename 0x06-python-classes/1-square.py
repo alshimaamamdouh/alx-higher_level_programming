b10e259d0d5314c894168e02783235bbe558ab8e
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-21 09:38:30 -0800

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+'''square module'''
+
+
 class Square:
-    """Defines a square"""
+    '''A class that represents a square'''
+    
     def __init__(self, size):
         """Initialises the data"""
         self.__size = size
