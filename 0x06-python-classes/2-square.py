76034c7518ee7596d604af9a66276594848722f4
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-21 09:58:09 -0800

    up

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,11 +7,13 @@ class Square:
     def __init__(self, size):
         """Initialises the data"""
         self.__size = size
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def calculate_area(self):
+
         """
         Calculates the area of the square.
 
