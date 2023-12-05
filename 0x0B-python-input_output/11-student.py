0f0982bdfa6755d3e019ff684919d0f866aae87b
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-12-05 12:30:08 -0800

    task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+task 11
+"""
+
+
+class Student:
+    """Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ set """
+        if attrs is None:
+            return self.__dict__
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return new_dictionary
+
+    def reload_from_json(self, json):
+        """ load """
+        for key, value in json.items():
+            setattr(self, key, value)
