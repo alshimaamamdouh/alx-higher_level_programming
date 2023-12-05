696bdbe5236458a47b285c81bcc1deb560527d29
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-12-05 09:39:31 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ task 1
 def write_file(filename="", text=""):
     """ write in file"""
     with open(filename, "w", encoding="utf-8") as file_w:
-            num = file_w.write(text)
-            return num
+        num = file_w.write(text)
+        return num
