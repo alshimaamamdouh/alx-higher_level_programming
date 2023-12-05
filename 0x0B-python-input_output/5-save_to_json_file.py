30fc1873e9d67bfe1630a92ec6e332c246717382
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-12-05 10:51:46 -0800

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+task 5
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    import json
+    with open(filename, 'w', encoding='utf-8') as file_j:
+        json.dump(my_obj, file_j)
