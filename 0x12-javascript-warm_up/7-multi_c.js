865bd44f36173cb09acc64f9da0b4eac6809ecf5
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-02-12 14:37:52 -0800

    whole

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) 
-{
-	console.log("Missing number of occurrences");
-} 
-else
-{
-	let num = parseInt(process.argv[2]);
-	let i = 0;
-	for (i = 0; i < num; i++)
-	{
-		console.log("C is fun");
-	}
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const num = parseInt(process.argv[2]);
+  let i = 0;
+  for (i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) 
-{
-	console.log("Missing size");
-} 
-else 
-{
-	const num = parseInt(process.argv[2]);
-	for (let i = 0; i < num; i++) 
-	{
-		let row = "";
-		for (let j = 0; j < num; j++) 
-		{
-			row += "X";
-		}
-		console.log(row);
-	}
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing size');
+} else {
+  const num = parseInt(process.argv[2]);
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
 }
