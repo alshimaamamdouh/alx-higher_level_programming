015fb6aa5c565b2f8c6345f16f2614a9469274cc
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-02-12 22:09:43 -0800

    up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 for (i = 0; i <= 2; i++) {
   console.log(arr[i]);
