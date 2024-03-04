fdf981e6fd551fe3f763b1bcc25067fad69886bf
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-03-04 14:33:37 -0800

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+
+import urllib.request
+with urllib.request.urlopen(url) as response:
+    content = response.read().decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
